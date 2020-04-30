01acb6144bd231bb15c2ea4b199b3ba6efb5ee60
Divepit <thedivepit@gmail.com>
2020-04-30 19:23:18 +0200

    adding dist folder to gitignore

---
+++ dist/precache-manifest.2b95ce212c62d83e7092e61f46e662ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10da43b0bad02f121f09",
+    "url": "/~marcotr/redirector/css/app.3b72bb5a.css"
+  },
+  {
+    "revision": "a2a1c6ac2d5877a7d158",
+    "url": "/~marcotr/redirector/css/chunk-vendors.a9263a26.css"
+  },
+  {
+    "revision": "9b7373ca2810207a2ea7c96caf061678",
+    "url": "/~marcotr/redirector/index.html"
+  },
+  {
+    "revision": "10da43b0bad02f121f09",
+    "url": "/~marcotr/redirector/js/app.e00060f9.js"
+  },
+  {
+    "revision": "a2a1c6ac2d5877a7d158",
+    "url": "/~marcotr/redirector/js/chunk-vendors.cc07dc51.js"
+  },
+  {
+    "revision": "f521ddca2ce7f0d59b2aef68d426b8aa",
+    "url": "/~marcotr/redirector/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/~marcotr/redirector/robots.txt"
+  }
+]);
